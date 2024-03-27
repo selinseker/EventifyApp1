025397393599b233a171804c851a8ef13be75a9f
Selin Seker <selinseker001@gmail.com>
2024-03-28 01:08:17 +0300

    initial commit

---
+++ EventifyApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventifyApp1
+//
+//  Created by Selin Şeker on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
