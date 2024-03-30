1df03dba03ef59bb948cc5ff12d26f17febbcdc5
Selin Seker <selinseker001@gmail.com>
2024-03-31 00:45:35 +0300

    Fotoğraf yükleme arayüzü ve Fairebase storage entegrasyonu yapıldı. Alınan görseller uuid leri saklanarak storage içine kayıt ediliyor.

--- EventifyApp1/SettingsViewController.swift
+++ EventifyApp1/SettingsViewController.swift
@@ -31,15 +31,5 @@ class SettingsViewController: UIViewController {
         performSegue(withIdentifier: "toFirstViewController", sender: nil)
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- EventifyApp1/UploadViewController.swift
+++ EventifyApp1/UploadViewController.swift
@@ -6,24 +6,82 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
+import FirebaseCore
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+let uuid = UUID().uuidString
 
+class UploadViewController: UIViewController, PHPickerViewControllerDelegate, UINavigationBarDelegate {
+    
+    @IBOutlet weak var yorumTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec) )
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func gorselSec(){
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        
+        let PHPickerVC = PHPickerViewController(configuration: config)
+        PHPickerVC.delegate = self
+        self.present(PHPickerVC, animated: true)
     }
-    */
-
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        guard let selectedImage = results.first else { return }
+        
+        selectedImage.itemProvider.loadObject(ofClass: UIImage.self) { (object, error) in
+            if let error = error {
+                print("Error loading image: \(error.localizedDescription)")
+            } else if let image = object as? UIImage {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        dismiss(animated: true)
+        
+    }
+    
+    @IBAction func imageUploadTiklandi(_ sender: Any){
+        
+        let storage = Storage.storage()
+        let storageReferance = storage.reference()
+        
+        let mediaFolder = storageReferance.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { storagemetadata, error in
+                if error != nil{
+                    self.hataMesajiGoster(title: "Hata", message: error?.localizedDescription ?? "Hata aldınız tekrar deneyiniz")
+                } else{
+                    imageReference.downloadURL { url, error in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl ?? "defaultvalue")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func hataMesajiGoster(title: String,message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
