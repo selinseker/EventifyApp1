fe0485130c493797d5341aa202d29c33be1a60ea
Selin Seker <selinseker001@gmail.com>
2024-04-02 19:58:16 +0300

    Login ve signup ekranları ayrıldı. Kullanıcılardan kayıt esnasında alınan bilgiler Firestore'da Users koleksiyonu içine kayıt edildi. Feed ekranında gönderinin üstündeki yazı "username" olacak şekilde değiştirildi.

---
+++ EventifyApp1/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  EventifyApp1
+//
+//  Created by Selin Şeker on 31.03.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
