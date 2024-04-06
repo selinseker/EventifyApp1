62fe7f0de22cb077603eea7f2989e9ce055a3860
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2024-04-06 23:57:50 +0300

    Update FeedViewController.swift

--- EventifyApp1/FeedViewController.swift
+++ EventifyApp1/FeedViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import FirebaseFirestore
+import FirebaseAuth // FirebaseAuth ekleyin
 import SDWebImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -7,141 +8,87 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var gorselDizisi = [String]()
     var yorumDizisi = [String]()
     var usernameDizisi = [String]()
-
+    var kullaniciID: String? // Kullanıcı ID'si
     
     @IBOutlet weak var feedTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      
-        firebaseVerileriAl()
         
-    
+        if let currentUser = Auth.auth().currentUser {
+            kullaniciID = currentUser.uid
+        }
+        
+        firebaseVerileriAl()
         feedTableView.delegate = self
         feedTableView.dataSource = self
     }
     
-    func firebaseVerileriAl(){
+    func firebaseVerileriAl() {
         let firestoreDatabase = Firestore.firestore()
         
-        firestoreDatabase.collection("Users").addSnapshotListener { snapshot, error in
-            if error != nil {
-                self.hataMesaji(title: "Hata", message: error?.localizedDescription ?? "Hata oluştu.")
-            } else {
-                if snapshot?.isEmpty != true && snapshot != nil{
-                    
-                    self.usernameDizisi.removeAll(keepingCapacity: false)
-                    self.gorselDizisi.removeAll(keepingCapacity: false)
-                    self.yorumDizisi.removeAll(keepingCapacity: false)
-                    
-                    for document in snapshot!.documents{
-                        if let username = document.get("username") as? String{
-                            self.usernameDizisi.append(username)
-                        }
-                    }
-                }
-            }
-            
-            
-            firestoreDatabase.collection("Post").addSnapshotListener { snapshot, error in
-                if error != nil {
-                    self.hataMesaji(title: "Hata", message: error?.localizedDescription ?? "Hata oluştu.")
-                } else{
-                    if snapshot?.isimport UIKit
-import FirebaseFirestore
-import SDWebImage
-
-
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    var gorselDizisi = [String]()
-    var yorumDizisi = [String]()
-    var usernameDizisi = [String]()
-
-    
-    @IBOutlet weak var feedTableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-      
-        firebaseVerileriAl()
-        
-        
-    
-        feedTableView.delegate = self
-        feedTableView.dataSource = self
-    }
-    
-    func firebaseVerileriAl(){
-        let firestoreDatabase = Firestore.firestore()
-        
-        firestoreDatabase.collection("Users").addSnapshotListener { snapshot, error in
-            if error != nil {
-                self.hataMesaji(title: "Hata", message: error?.localizedDescription ?? "Hata oluştu.")
-            } else {
-                if snapshot?.isEmpty != true && snapshot != nil{
-                    
-                    self.usernameDizisi.removeAll(keepingCapacity: false)
-                    self.gorselDizisi.removeAll(keepingCapacity: false)
-                    self.yorumDizisi.removeAll(keepingCapacity: false)
-                    
-                    for document in snapshot!.documents{
-                        if let username = document.get("username") as? String{
-                            self.usernameDizisi.append(username)
-                        }
+        if let uid = kullaniciID {
+            let kullaniciReferans = firestoreDatabase.collection("Users").document(uid)
+            kullaniciReferans.getDocument { (document, error) in
+                if let document = document, document.exists {
+                    if let username = document.get("username") as? String {
+                        self.usernameDizisi.append(username)
                         
-                    }
-                }
-            }
-            
-            
-            firestoreDatabase.collection("Post").addSnapshotListener { snapshot, error in
-                if error != nil {
-                    self.hataMesaji(title: "Hata", message: error?.localizedDescription ?? "Hata oluştu.")
-                } else{
-                    if snapshot?.isEmpty != true && snapshot != nil {
-                        for document in snapshot!.documents {
-                            if let gorselurl = document.get("gorselurl") as? String{
-                                self.gorselDizisi.append(gorselurl)
-                            }
-                            if let yorum = document.get("yorum") as? String{
-                                self.yorumDizisi.append(yorum)
-                            }
-                            
-                        }
                         DispatchQueue.main.async {
                             self.feedTableView.reloadData()
-                            
                         }
+                    }
+                } else {
+                    print("Kullanıcı belirtilen ID ile bulunamadı: \(error?.localizedDescription ?? "Bilgi yok")")
+                }
+            }
+        }
+        
+       
+        firestoreDatabase.collection("Post").addSnapshotListener { snapshot, error in
+            if error != nil {
+                self.hataMesaji(title: "Hata", message: error?.localizedDescription ?? "Hata oluştu.")
+            } else {
+                if let snapshot = snapshot, !snapshot.isEmpty {
+                    self.gorselDizisi.removeAll()
+                    self.yorumDizisi.removeAll()
+                    
+                    for document in snapshot.documents {
+                        if let gorselurl = document.get("gorselurl") as? String {
+                            self.gorselDizisi.append(gorselurl)
+                        }
+                        if let yorum = document.get("yorum") as? String {
+                            self.yorumDizisi.append(yorum)
+                        }
+                    }
+                    // Tabloyu yenile
+                    DispatchQueue.main.async {
+                        self.feedTableView.reloadData()
                     }
                 }
             }
         }
     }
     
-        
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return yorumDizisi.count
-     
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = feedTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
         cell.YorumText.text = yorumDizisi[indexPath.row]
-        cell.feedUsernameField.text = usernameDizisi[indexPath.row]
+        cell.feedUsernameField.text = usernameDizisi.first ?? "" // İlk kullanıcı adını al
         cell.postImageView.sd_setImage(with: URL(string: self.gorselDizisi[indexPath.row]))
         return cell
     }
     
-    func hataMesaji(title: String,message: String){
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+    func hataMesaji(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okButton)
         self.present(alert, animated: true, completion: nil)
     }
-    
 }
+
