8a2f4fbe551de11a153dc459c6abb62d3155949b
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2024-04-07 17:14:11 +0300

    Update ViewController.swift

--- EventifyApp1/ViewController.swift
+++ EventifyApp1/ViewController.swift
@@ -11,37 +11,22 @@ import FirebaseAuth
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-    @IBAction func signupTiklandi(_ sender: Any) {
-        
-        if emailTextField.text != " " && passwordTextField.text != " " {
-            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authdataresult, error in
-                if error != nil{
-                    self.hataMesajı(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata aldınız tekrar deneyin")
-                }
-                else{
-                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
-                }
-            }
-        }
-        else{
-            hataMesajı(titleInput: "Hata", messageInput: "Email ve şifre giriniz")
-        }
-    }
-    
     @IBAction func loginTiklandi(_ sender: Any) {
         
-        if emailTextField.text != " " && passwordTextField.text != " " {
+        if emailTextField.text != "" && passwordTextField.text != "" {
             Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { authdataresult, error in
                 if error != nil{
-                    self.hataMesajı(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata aldınız, tekrar deneyiniz")
+                    self.hataMesaji(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata aldınız, tekrar deneyiniz")
                 }
                 else{
                     self.performSegue(withIdentifier: "toFeedVC", sender: nil)
@@ -49,16 +34,19 @@ class ViewController: UIViewController {
             }
         }
         else{
-            self.hataMesajı(titleInput: "Hata", messageInput: "Hata aldınız tekrar deneyin")
+            self.hataMesaji(titleInput: "Hata", messageInput: "Gerekli bilgileri giriniz")
         }
     }
     
-    func hataMesajı(titleInput: String, messageInput:String) {
+    func hataMesaji(titleInput: String, messageInput:String) {
         let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
         alert.addAction(okButton)
         self.present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func toSignupTiklandi(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
 }
 
